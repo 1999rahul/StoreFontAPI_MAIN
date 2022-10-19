397c8a644976c1c7ff5d5524d94a2572b038efaa
Rahul <1999rahul.py@gmail.com>
2022-10-19 10:01:09 +0530

    Added class based viwes without mixins

--- store/views.py
+++ store/views.py
@@ -1,37 +1,39 @@
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-@api_view(['GET','POST'])
-def product_list(request):
-    if request.method=='GET':
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
+class ProductDetail(APIView):
+    def get(self,request,id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method=='POST':
-        serializer=ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-@api_view(['GET','PUT','DELETE'])
-def product_detail(request, id):
-    product=get_object_or_404(Product,pk=id)
-    if request.method=='GET':
-             serializer = ProductSerializer(product)
-             return Response(serializer.data)
-    elif request.method=='PUT':
-        serializer=ProductSerializer(product,data=request.data)
+    def put(self,request,id):
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
-    elif request.method=='DELETE':
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def delete(self,request,id):
+        product = get_object_or_404(Product, pk=id)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+class ProductList(APIView):
+    def get(self,request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Create your views here.
