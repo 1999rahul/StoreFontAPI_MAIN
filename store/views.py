5e2ca827297ea1c776873d2d73136bf144c2e0bb
Rahul <1999rahul.py@gmail.com>
2022-10-20 00:05:46 +0530

    added filters

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from .models import Product,Review
 from .serializers import ProductSerializer,ReviewSerializer
@@ -5,6 +6,9 @@ from .serializers import ProductSerializer,ReviewSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields=['collection_id']
+
 class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
