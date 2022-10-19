d477b3b631784e0fd8de61316c8eefb7f55e9902
Rahul <1999rahul.py@gmail.com>
2022-10-19 07:29:18 +0530

    used meta class to create model serilizer

--- store/serializers.py
+++ store/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from decimal import Decimal
 from .models import Product,Collection
 
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Collection
+        fields=['id','title']
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','description','price','inventory','price_with_tax','collection']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
-    collection=serializers.StringRelatedField()
-
     def calculate_tax(self, product: Product):
         return product.price*Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,26 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-
-
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
-
-
-@api_view()
-def product_detail(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
+    if request.method=='GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method=='POST':
+        serializer=ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET','PUT','DELETE'])
+def product_detail(request, id):
+    product=get_object_or_404(Product,pk=id)
+    if request.method=='GET':
+             serializer = ProductSerializer(product)
+             return Response(serializer.data)
+    elif request.method=='PUT':
+        serializer=ProductSerializer(product,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    elif request.method=='DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 # Create your views here.
