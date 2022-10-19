80ce3efc56c1e8fb6edef37d9e2ddd4fe3dee287
Rahul <1999rahul.py@gmail.com>
2022-10-19 10:46:05 +0530

    using generic view methods to get or create product list ^T

--- store/views.py
+++ store/views.py
@@ -1,11 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 class ProductDetail(APIView):
+
     def get(self,request,id):
         product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
@@ -22,18 +24,13 @@ class ProductDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ProductList(APIView):
-    def get(self,request):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
-    def post(self,request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProductList(ListCreateAPIView):
+
+    def get_queryset(self):
+        return Product.objects.all()
+    def get_serializer_class(self):
+        return ProductSerializer
+
 
 
 # Create your views here.
